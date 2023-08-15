2f78fbf6f27859def31bb84b07cb7e350e172b4e
smartysmart47 <139089002+smartysmart47@users.noreply.github.com>
2023-08-15 05:58:40 +0000

    second commit done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,21 @@
+const accountId = 1425
+let accountEmail = "shankerjoshi45@gmail.com"
+var accountPassword = "12345"
+accountCity = "Uttarakhand"
+
+let accountState;
+
+//  var ab use nahi krte hai...
+
+accountEmail = "sh5464@gmal.com"
+accountPassword = "784656"
+accountCity = "Haridwar"
+
+// let--> use --> 
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
