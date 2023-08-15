54b5fc7571b802d255ae63d14a8a93b6a8af6021
smartysmart47 <139089002+smartysmart47@users.noreply.github.com>
2023-08-15 17:10:06 +0530

    Create 03_conversion.js

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,38 @@
+// let score = "25abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "shanker"
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNum = Number(score)
+// console.log(typeof valueInNum);
+// console.log(valueInNum);
+// console.log(typeof (valueInNum));
+
+
+/*
+NaN = not a number
+" 33 " --> 33
+" 33 abc " --> NaN
+true - -> 1
+false - -> 0
+
+*/
+
+// let isLogedIn = 1
+// let isLogedIn = 0
+let isLogedIn = "shanker"
+
+// let booleanIsLogged = Boolean(isLogedIn)
+// console.log(booleanIsLogged);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
