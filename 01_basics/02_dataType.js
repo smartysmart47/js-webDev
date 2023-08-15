b1f4bd64aba8b0538c00644476f26ddf418a1f8c
smartysmart47 <139089002+smartysmart47@users.noreply.github.com>
2023-08-15 11:55:01 +0530

    Create 02_dataType.js

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,24 @@
+"use strict";  // treat all JS code as newer version
+
+// alert( "The sum is : " , 6+3 ); // we are using nodejs not brower
+
+// console.log(9+6);
+// console.log("This is shanker joshi. Have you any problem when i am there");
+let name = "shanker"
+let age = 18
+let isTrue = false
+
+/* Primitive DataType
+number
+null
+string
+boolean
+undefined
+bigint
+symbol
+
+*/
+
+console.log(typeof null);
+console.log(typeof undefined);
+// null is object ds type
